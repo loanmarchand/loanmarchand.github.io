981ad5341199654b8b1a5fb4f1eb3219714d1df3
loanmarchand <113551207+loanmarchand@users.noreply.github.com>
2023-04-06 14:41:16 +0200

    Add files

---
+++ style.js
@@ -0,0 +1,4 @@
+document.getElementById("submit-button").addEventListener("click", function () {
+    document.getElementById("rickroll").style.display = "block";
+    document.getElementById("video").requestFullscreen();
+});
